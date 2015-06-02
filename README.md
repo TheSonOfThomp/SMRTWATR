81b8ecdc98177bb3cc38b8a39b13df6a06579a64
Josh Bradshaw <joshbradshaw11@gmail.com>
2015-06-01 22:41:25 -0400

    setup repository

---
+++ README.md
@@ -0,0 +1,10 @@
+SYDE 361 - Interactive Fountain Project
+
+This repository includes the source code for the web applicaition, and the embedded software for our interactive fountain prototype.
+
+Group Members:
+Josh Bradshaw
+Isaac Hunter
+Adam Thompson
+Emma Cooper
+Shubh Jagani
---
+++ controls/servo-controller/servo-controller.ino
@@ -0,0 +1,29 @@
+// Sweep
+// by BARRAGAN <http://barraganstudio.com> 
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo.attach(20);  // attaches the servo on pin 20 
+} 
+ 
+ 
+void loop() 
+{ 
+  for(pos = 10; pos < 170; pos += 1)  // goes from 10 degrees to 170 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+} 
