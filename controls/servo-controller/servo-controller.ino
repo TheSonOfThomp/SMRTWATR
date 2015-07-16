bf87ff1d72e50b27585a42c3d3cf2f8e5f36460a
Shubh Jagani <shubh.jagani@gmail.com>
2015-07-15 21:33:46 -0400

    Winner and rest fountain sequences.
    Added the 4 end game sequences.

--- controls/servo-controller/servo-controller.ino
+++ controls/servo-controller/servo-controller.ino
@@ -7,8 +7,10 @@ const bool CCW = false;
 
 const int BUFFER_LEN = 6;
 const int SERVO_DELAY = 25; // ms per degree
-const int SERVO_LL = 10;
-const int SERVO_RL = 170;
+const int SERVO_LL = 0;
+const int SERVO_RL = 90;
+const int CJET_QHEIGHT = 3; //height of the center jet during the quiz (see discrete_jet_heights array below --> 4095)
+int timecount = 0; //counts the number of 25ms increments that have passed. 
 char instr_buff[BUFFER_LEN];
 char prev_state;
 int DISCRETE_JET_HEIGHTS[4] = {600, 1100, 2500, 4095}; // discrete heights to be used during quiz mode
@@ -23,7 +25,7 @@ int jet_pins[5] = {23, 22, 20, 17, 16};
 // bottom-left, top-left, top-right, bottom-right (ccw)
 Servo servo[4];
 int servo_pins[4] = {3, 4, 6, 9};
-int servo_pos[4] = {0, 0, 0, 0};
+int servo_pos[4] = {0, 0, 0, 0}; 
 bool servo_sweep[4] = {CW, CW, CW, CW};
 
 void setup() {
@@ -66,27 +68,66 @@ void loop() {
 }
 
 void routine_setup() {
+    int p0 = instr_buff[2] - '0';
+    int p1 = instr_buff[3] - '0';
+    int p2 = instr_buff[4] - '0';
+    int p3 = instr_buff[5] - '0';
+    int p4 = CJET_QHEIGHT;
+    
     switch (instr_buff[1]) {
     case '1':
-        set_discrete_pump_heights();
+        set_discrete_pump_heights(p0, p1, p2, p3, p4);
         servo_sweep[0] = servo_sweep[1] = servo_sweep[2] = servo_sweep[3] = CW;
         zero_servos();
         break;
     case '2':
         servo_sweep[0] = servo_sweep[2] = CW;
         servo_sweep[1] = servo_sweep[3] = CCW;
-        set_discrete_pump_heights();
+        set_discrete_pump_heights(p0, p1, p2, p3, p4);
         zero_servos();
         break;
     case '3':
-        set_discrete_pump_heights();
+        set_discrete_pump_heights(p0, p1, p2, p3, p4);
         servo_sweep[0] = servo_sweep[1] = servo_sweep[2] = servo_sweep[3] = CW;
         zero_servos();
         break;
-    case '4':
+     case '4':
+        //Serial.printf("final routine 1 setup \n");
+        servo_sweep[0] = servo_sweep[1] = servo_sweep[2] = servo_sweep[3] = CW;
+        //serial.print(servo_sweep[0]);
+        set_discrete_pump_heights(0, 0, 0, 0, 3);
+        zero_servos();
+        break;
+     case '5':
+        //serial.printf("final routine 2 setup \n");
+        servo_sweep[0] = servo_sweep[1] = CCW;
+        servo_sweep[2] = servo_sweep[3] = CW;
+        set_discrete_pump_heights(0, 0, 0, 0, 3);
+        zero_servos();
+        break;
+     case '6':
+        //serial.printf("final routine 3 setup \n");
+        servo_sweep[0] = servo_sweep[1] = CW;
+        servo_sweep[2] = servo_sweep[3] = CCW;
+        set_discrete_pump_heights(0, 0, 0, 0, 0);
+        zero_servos();
+        break;
+     case '7':
+        //serial.printf("final routine 4 setup \n");
+        servo_sweep[0] = servo_sweep[1] = CCW;
+        servo_sweep[2] = servo_sweep[3] = CW;
+        set_discrete_pump_heights(0, 0, 0, 0, 0);
+        zero_servos();
+        break;
+    case '8':
+        servo_sweep[0] = servo_sweep[1] = servo_sweep[2] = servo_sweep[3] = CW;
+        set_discrete_pump_heights(3, 3, 3, 3, 3);
+        zero_servos();
         break;
     }
 }
+
+
 
 void routine() {
     switch (instr_buff[1]) {
@@ -99,22 +140,56 @@ void routine() {
         }
         break;
     case '4':
+        if (d.Timeout()) {
+            win_seq1();
+            d.Delay(SERVO_DELAY);
+        }
+        break;    
+    case '5':
+        if (d.Timeout()) {
+            win_seq2();
+            d.Delay(SERVO_DELAY);
+        }
+        break; 
+    case '6':
+        if (d.Timeout()) {
+            win_seq3();
+            d.Delay(SERVO_DELAY);
+        }
         break;
+    case '7':
+        if (d.Timeout()) {
+            win_seq4();
+            d.Delay(SERVO_DELAY);
+        }
+        break;
+    case '8':
+        if (d.Timeout()) {
+            rest_seq();
+            d.Delay(SERVO_DELAY);
+        }
+        break;    
     }
 }
 
 void quiz_score_routine() {
+    int p0 = instr_buff[2] - '0';
+    int p1 = instr_buff[3] - '0';
+    int p2 = instr_buff[4] - '0';
+    int p3 = instr_buff[5] - '0';
+    int p4 = CJET_QHEIGHT;
     // called when the score changes
-    set_discrete_pump_heights();
+    set_discrete_pump_heights(p0, p1, p2, p3, p4);
 }
 
-void set_discrete_pump_heights() {
-    // set discrete jet heights (1, 2, 3, 4) during the quiz
-    for (int ii = 0; ii < 4; ii++) {
-        jet_height[ii] = DISCRETE_JET_HEIGHTS[instr_buff[ii + 2] - '0'];
-        Serial.println(jet_height[ii]);
-        analogWrite(jet_pins[ii], jet_height[ii]);
-    }
+void set_discrete_pump_heights(int p0, int p1, int p2, int p3, int p4) {
+   ////serial.printf("discrete jet heights: %d, %d, %d, %d, %d", p0, p1, p2, p3);  
+    analogWrite(jet_pins[0], DISCRETE_JET_HEIGHTS[p0]);
+    analogWrite(jet_pins[1], DISCRETE_JET_HEIGHTS[p1]);
+    analogWrite(jet_pins[2], DISCRETE_JET_HEIGHTS[p2]);
+    analogWrite(jet_pins[3], DISCRETE_JET_HEIGHTS[p3]);
+    analogWrite(jet_pins[4], DISCRETE_JET_HEIGHTS[p4]);
+    
 }
 
 void zero_servos() {
@@ -142,10 +217,10 @@ void sweep_servos() {
     // sweep the servos back and forth based on the initial condition given in servo_sweep
     for (int ii=0; ii<4; ii++) {
         // reverse direction if necessary
-        if (servo_sweep[ii]==CW && servo_pos[ii] > SERVO_RL) {
+        if (servo_sweep[ii]==CW && servo_pos[ii] >= SERVO_RL) {
             servo_sweep[ii] = CCW;
         }
-        if (servo_sweep[ii]==CCW && servo_pos[ii] < SERVO_LL) {
+        if (servo_sweep[ii]==CCW && servo_pos[ii] <= SERVO_LL) {
             servo_sweep[ii] = CW;
         }
         // advance servos if necessary
@@ -158,3 +233,255 @@ void sweep_servos() {
         }
     }
 }
+
+void rest_seq(){
+   for (int ii=0; ii<4; ii++) {
+        // reverse direction if necessary
+        if (servo_sweep[ii]==CW && servo_pos[ii] >= SERVO_RL) {
+            servo_sweep[ii] = CCW;
+        }
+        if (servo_sweep[ii]==CCW && servo_pos[ii] <= SERVO_LL) {
+            servo_sweep[ii] = CW;
+        }
+        // advance servos if necessary
+        if (servo_sweep[ii] == CW) {
+            servo_pos[ii] += 1;
+            servo[ii].write(servo_pos[ii]);
+        } else {
+            servo_pos[ii] -= 1;
+            servo[ii].write(servo_pos[ii]);
+        }
+    }
+}
+void win_seq1(){
+  // first option 
+  // water goes from one pump to another (hopefully)
+  
+  if (servo_pos[0] < SERVO_RL && servo_sweep[0] ){
+    //serial.printf("\n m1 moving right");
+    servo_pos[0] += 1;
+    servo[0].write(servo_pos[0]); 
+    if (servo_pos[3] > SERVO_LL)
+    {
+      servo_sweep[3] = CCW;
+      servo_pos[3] -= 1;
+      //serial.printf("\n m4 moving back left");
+      servo[3].write(servo_pos[3]);
+    }
+    set_discrete_pump_heights(3, 0, 0, 0, 3);
+  }   
+  else if (servo_pos[1] < SERVO_RL && servo_sweep[1] == CW){
+    servo_pos[1] += 1;
+    servo[1].write(servo_pos[1]); 
+    //serial.printf("\n m2 moving right");
+    servo_sweep[0] = CCW;
+    if (servo_pos[0] > SERVO_LL){
+      servo_pos[0] -= 1;
+      servo[0].write(servo_pos[0]);
+      //serial.printf("\n m1 moving back left");
+    }
+    set_discrete_pump_heights(0, 3, 0, 0, 3);
+  }
+  else if (servo_pos[2] < SERVO_RL && servo_sweep[2] == CW){
+    servo_pos[2] += 1;
+    servo[2].write(servo_pos[2]);
+    //serial.printf("\n m3 moving right");
+    servo_sweep[1] = CCW;
+    if (servo_pos[1] > SERVO_LL){
+      servo_pos[1] -= 1;
+      servo[1].write(servo_pos[1]); 
+      //serial.printf("\n m2 moving back left");
+    }
+    set_discrete_pump_heights(0, 0, 3, 0, 3);
+  }
+  
+  else if (servo_pos[3] < SERVO_RL && servo_sweep[3] == CW){
+    servo_pos[3] += 1;
+    servo[3].write(servo_pos[3]); 
+    //serial.printf("\n m4 moving right");
+    servo_sweep[2] = CCW;
+    if (servo_pos[2] > SERVO_LL){
+      servo_pos[2] -= 1;
+      servo[2].write(servo_pos[2]);
+      //serial.printf("\n m3 moving back left");
+    }
+    set_discrete_pump_heights(0, 0, 0, 3, 3);
+  }
+  else{
+    servo_sweep[0] = CW;
+    servo_sweep[1] = CW;
+    servo_sweep[2] = CW;
+    servo_sweep[3] = CW;
+    set_discrete_pump_heights(0, 0, 0, 0, 3);
+  }
+}
+
+void win_seq2(){
+  //serial.print("timecount:"+ timecount);
+  if (timecount == 200){ //first 5 seconds
+    set_discrete_pump_heights(3, 3, 3, 3, 0);
+  }
+  else if (timecount == 400){
+    set_discrete_pump_heights(0, 0, 0, 0, 3);
+  }
+  else if (timecount == 600){
+    set_discrete_pump_heights(3, 0, 3, 0, 3);
+  }
+  else if (timecount == 800) {
+    set_discrete_pump_heights(0, 3, 0, 3, 3);
+  }
+  else if (timecount == 1000) {
+    set_discrete_pump_heights(3, 3, 3, 3, 3);
+  }
+  else if (timecount == 1200) {
+    timecount = 0;
+  }
+  timecount += 1;
+  for (int ii=0; ii<4; ii++) {
+        //serial.printf("\n in for loop");
+        // reverse direction if necessary
+        if (servo_sweep[ii]==CW && servo_pos[ii] >= SERVO_RL) {
+            servo_sweep[ii] = CCW;
+        }
+        if (servo_sweep[ii]==CCW && servo_pos[ii] <= SERVO_LL) {
+            servo_sweep[ii] = CW;
+        }
+        // advance servos if necessary
+        if (servo_sweep[ii] == CW) {
+            servo_pos[ii] += 1;
+            servo[ii].write(servo_pos[ii]);
+        } 
+        else {
+            servo_pos[ii] -= 1;
+            servo[ii].write(servo_pos[ii]);
+        }
+    }
+  
+}
+
+void win_seq3(){
+  //serial.print("timecount:"+ timecount);
+  if (timecount == 200){ //first 5 seconds
+    set_discrete_pump_heights(3, 3, 3, 3, 0);
+  }
+  else if (timecount == 400){
+    set_discrete_pump_heights(0, 0, 0, 0, 3);
+  }
+  else if (timecount == 600){
+    set_discrete_pump_heights(3, 0, 3, 0, 3);
+  }
+  else if (timecount == 800) {
+    set_discrete_pump_heights(0, 3, 0, 3, 3);
+  }
+  else if (timecount == 1000) {
+    set_discrete_pump_heights(3, 3, 3, 3, 3);
+  }
+  else if (timecount == 1200) {
+    timecount = 0;
+  }
+  timecount += 1;
+  
+  
+  for (int ii=1; ii<=3; ii+=2) {
+        //serial.printf("\n in for loop");
+        // reverse direction if necessary
+        if (servo_sweep[1]==CW && servo_pos[ii] >= SERVO_RL/2) {
+            servo_sweep[ii] = CCW;
+        }
+        if (servo_sweep[ii]==CCW && servo_pos[ii] <= SERVO_LL/2) {
+            servo_sweep[ii] = CW;
+        }
+        // advance servos if necessary
+        if (servo_sweep[ii] == CW) {
+            servo_pos[ii] += 1;
+            servo[ii].write(servo_pos[ii]);
+        } 
+        else {
+            servo_pos[ii] -= 1;
+            servo[ii].write(servo_pos[ii]);
+        }
+    }
+  for (int ii=0; ii<=2; ii+=2) {
+        //serial.printf("\n in for loop");
+        // reverse direction if necessary
+        if (servo_sweep[ii]==CW && servo_pos[ii] >= SERVO_RL) {
+            servo_sweep[ii] = CCW;
+        }
+        if (servo_sweep[ii]==CCW && servo_pos[ii] <= SERVO_LL) {
+            servo_sweep[ii] = CW;
+        }
+        // advance servos if necessary
+        if (servo_sweep[ii] == CW) {
+            servo_pos[ii] += 1;
+            servo[ii].write(servo_pos[ii]);
+        } 
+        else {
+            servo_pos[ii] -= 1;
+            servo[ii].write(servo_pos[ii]);
+        }
+    }   
+} 
+
+void win_seq4(){
+  //serial.print("timecount:"+ timecount);
+  if (timecount == 200){ //first 5 seconds
+    set_discrete_pump_heights(3, 3, 3, 3, 0);
+  }
+  else if (timecount == 400){
+    set_discrete_pump_heights(0, 0, 0, 0, 3);
+  }
+  else if (timecount == 600){
+    set_discrete_pump_heights(3, 0, 3, 0, 3);
+  }
+  else if (timecount == 800) {
+    set_discrete_pump_heights(0, 3, 0, 3, 3);
+  }
+  else if (timecount == 1000) {
+    set_discrete_pump_heights(3, 3, 3, 3, 3);
+  }
+  else if (timecount == 1200) {
+    timecount = 0;
+  }
+  timecount += 1;
+  
+  
+  for (int ii=1; ii<=3; ii+=2) {
+        //Serial.printf("\n in for loop");
+        // reverse direction if necessary
+        if (servo_sweep[1]==CW && servo_pos[ii] >= SERVO_RL) {
+            servo_sweep[ii] = CCW;
+        }
+        if (servo_sweep[ii]==CCW && servo_pos[ii] <= SERVO_LL) {
+            servo_sweep[ii] = CW;
+        }
+        // advance servos if necessary
+        if (servo_sweep[ii] == CW) {
+            servo_pos[ii] += 1;
+            servo[ii].write(servo_pos[ii]);
+        } 
+        else {
+            servo_pos[ii] -= 5;
+            servo[ii].write(servo_pos[ii]);
+        }
+    }
+  for (int ii=0; ii<=2; ii+=2) {
+        //Serial.printf("\n in for loop");
+        // reverse direction if necessary
+        if (servo_sweep[ii]==CW && servo_pos[ii] >= SERVO_RL) {
+            servo_sweep[ii] = CCW;
+        }
+        if (servo_sweep[ii]==CCW && servo_pos[ii] <= SERVO_LL) {
+            servo_sweep[ii] = CW;
+        }
+        // advance servos if necessary
+        if (servo_sweep[ii] == CW) {
+            servo_pos[ii] += 5;
+            servo[ii].write(servo_pos[ii]);
+        } 
+        else {
+            servo_pos[ii] -= 1;
+            servo[ii].write(servo_pos[ii]);
+        }
+    }   
+} 
+
