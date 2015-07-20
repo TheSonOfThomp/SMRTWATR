a06cddf6e1b59ff504b3c65e6c173e5dc47e5f82
Isaac Hunter <isaac.hunter.c@gmail.com>
2015-07-19 23:37:56 -0400

    more client.py

--- smrtwatr_client/client.py
+++ smrtwatr_client/client.py
@@ -1,18 +1,14 @@
 import websocket
-#import httplib
+import serial
  
 if __name__ == "__main__":
 
-	#conn  = httplib.HTTPConnection("localhost:9999")
-	#conn.request("GET", "/")
-	#resp = conn.getresponse()
-	#cookie = resp.getheader('Set-Cookie').split(';')[0]
-
-	#print cookie[6:]
-
 	websocket.enableTrace(True)
-	ws = websocket.create_connection("ws://localhost:9999/game/ws")
+	ws = websocket.create_connection("ws://smrtwatr.me:9999/game/ws")
 	print("connected")
+
+	#ser4 = serial.Serial('COM4', baudrate=9600, timeout=2)
+	ser5 = serial.Serial('COM5', baudrate=9600, timeout=2)
 	
 	score = 0
 	result = 1
@@ -23,13 +19,19 @@ if __name__ == "__main__":
 		if result.startswith('pi:'):
 			if result.startswith("end", 4):
 				print("Sending teensy: q80000")
+				#ser4.write('q80000')
+				ser5.write('q80000')
 			elif result.startswith("s:", 4):
 				seq = int(result.split("s:")[1][0]) + 4
 				print("Sending teensy: q" + str(seq) + "0000")
+				#ser4.write("q" + str(seq) + "0000")
+				ser5.write("q" + str(seq) + "0000")
 			elif result.startswith("c:", 4):
 				if result.split("c:",1)[1][0] == '1' :
 					score += 10 ** (4-int(result.split("p:",1)[1][0]))
 				print("Sending teensy: q" + result.split("q:",1)[1][0] + '%04d' % score )
+				#ser4.write("q" + result.split("q:",1)[1][0] + '%04d' % score )
+				ser5.write("q" + result.split("q:",1)[1][0] + '%04d' % score )
 
 
 	print("Closing")
